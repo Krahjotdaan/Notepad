4304b4de2a3c26eaf7cc9458ee58fe15153dfedf
Artem Ostapenko <a.ostapenko06@mail.ru>
2023-06-07 11:52:04 +0300

    Feat: added redo button, button handlers for change scale button, upgraded datetime button handler

--- Tekstoviy redaktor/MainWindow.xaml.cs
+++ Tekstoviy redaktor/MainWindow.xaml.cs
@@ -14,208 +14,235 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
-using System.Windows.Xps.Packaging;
 using System.Drawing.Printing;
 
 namespace Tekstoviy_redaktor
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        string Filename = null;
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		string Filename = null;
 
-        public MainWindow()
-        {
-            InitializeComponent();  
-        }
+		public MainWindow()
+		{
+			InitializeComponent();  
+		}
 
-        private void new_document_Click(object sender, RoutedEventArgs e)
-        {
-            if (Filename != null)
-            {
-                TextWriter textWriter = new StreamWriter(@Filename);
-                textWriter.Write(textbox.Text);
-                textWriter.Close();
-                textbox.Clear();
-                Filename = null;
-            }
-            else
-            {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
+		private void new_document_Click(object sender, RoutedEventArgs e)
+		{
+			if (Filename != null)
+			{
+				TextWriter textWriter = new StreamWriter(@Filename);
+				textWriter.Write(textbox.Text);
+				textWriter.Close();
+				textbox.Clear();
+				Filename = null;
+			}
+			else
+			{
+				SaveFileDialog saveFileDialog = new SaveFileDialog();
+				saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
 
-                if (saveFileDialog.ShowDialog() == true)
-                {
-                    Filename = saveFileDialog.FileName;
-                    TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
-                    textWriter.Write(textbox.Text);
-                    textWriter.Close();
-                    textbox.Clear();
-                    Filename = null;
-                }
-            }
-        }
+				if (saveFileDialog.ShowDialog() == true)
+				{
+					Filename = saveFileDialog.FileName;
+					TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
+					textWriter.Write(textbox.Text);
+					textWriter.Close();
+					textbox.Clear();
+					Filename = null;
+				}
+			}
+		}
 
-        private void new_window_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow window = new MainWindow();
-            window.Show(); 
-        }
+		private void new_window_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow window = new MainWindow();
+			window.Show(); 
+		}
 
-        private void open_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
-            
-            if (openFileDialog.ShowDialog() == true)
-            {
-                Filename = openFileDialog.FileName;
-                textbox.Text = File.ReadAllText(openFileDialog.FileName);
-            }
-        }
+		private void open_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
+			
+			if (openFileDialog.ShowDialog() == true)
+			{
+				Filename = openFileDialog.FileName;
+				textbox.Text = File.ReadAllText(openFileDialog.FileName);
+			}
+		}
 
-        private void save_Click(object sender, RoutedEventArgs e)
-        {
-            if (Filename != null)
-            {
-                TextWriter textWriter = new StreamWriter(@Filename);
-                textWriter.Write(textbox.Text);
-                textWriter.Close();
-            }
-            else
-            {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
+		private void save_Click(object sender, RoutedEventArgs e)
+		{
+			if (Filename != null)
+			{
+				TextWriter textWriter = new StreamWriter(@Filename);
+				textWriter.Write(textbox.Text);
+				textWriter.Close();
+			}
+			else
+			{
+				SaveFileDialog saveFileDialog = new SaveFileDialog();
+				saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
 
-                if (saveFileDialog.ShowDialog() == true)
-                {
-                    Filename = saveFileDialog.FileName;
-                    TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
-                    textWriter.Write(textbox.Text);
-                    textWriter.Close();
-                }
-            }
-        }
+				if (saveFileDialog.ShowDialog() == true)
+				{
+					Filename = saveFileDialog.FileName;
+					TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
+					textWriter.Write(textbox.Text);
+					textWriter.Close();
+				}
+			}
+		}
 
-        private void save_as_Click(object sender, RoutedEventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
+		private void save_as_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
 
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                Filename = saveFileDialog.FileName;
-                TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
-                textWriter.Write(textbox.Text);
-                textWriter.Close();
-            }
-        }
+			if (saveFileDialog.ShowDialog() == true)
+			{
+				Filename = saveFileDialog.FileName;
+				TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
+				textWriter.Write(textbox.Text);
+				textWriter.Close();
+			}
+		}
 
-        private void page_settings_Click(object sender, RoutedEventArgs e)
-        {
-            System.Windows.Forms.PageSetupDialog pageSetupDialog = new System.Windows.Forms.PageSetupDialog();
-            pageSetupDialog.PageSettings = new PageSettings();
+		private void page_settings_Click(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Forms.PageSetupDialog pageSetupDialog = new System.Windows.Forms.PageSetupDialog();
+			pageSetupDialog.PageSettings = new PageSettings();
 
-            pageSetupDialog.ShowNetwork = false;
+			pageSetupDialog.ShowNetwork = false;
 
-            System.Windows.Forms.DialogResult result = pageSetupDialog.ShowDialog();
-        }
+			System.Windows.Forms.DialogResult result = pageSetupDialog.ShowDialog();
+		}
 
-        private void printing_Click(object sender, RoutedEventArgs e)
-        {
-            PrintDialog printDialog = new PrintDialog();
-            printDialog.ShowDialog();
-        }
+		private void printing_Click(object sender, RoutedEventArgs e)
+		{
+			PrintDialog printDialog = new PrintDialog();
+			printDialog.ShowDialog();
+		}
 
-        private void exit_Click(object sender, RoutedEventArgs e)
-        {
-            if (Filename != null)
-            {
-                Close();
-            }
-            else
-            {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
+		private void exit_Click(object sender, RoutedEventArgs e)
+		{
+			if (Filename != null)
+			{
+				TextWriter textWriter = new StreamWriter(@Filename);
+				textWriter.Write(textbox.Text);
+				textWriter.Close();
+				Close();
+			}
+			else
+			{
+				SaveFileDialog saveFileDialog = new SaveFileDialog();
+				saveFileDialog.Filter = "Текстовые файлы |*.txt|Все файлы |*.*";
 
-                if (saveFileDialog.ShowDialog() == true)
-                {
-                    Filename = saveFileDialog.FileName;
-                    TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
-                    textWriter.Write(textbox.Text);
-                    textWriter.Close();
-                }
-                Close();
-            }
-        }
+				if (saveFileDialog.ShowDialog() == true)
+				{
+					Filename = saveFileDialog.FileName;
+					TextWriter textWriter = new StreamWriter(@saveFileDialog.FileName);
+					textWriter.Write(textbox.Text);
+					textWriter.Close();
+				}
+				Close();
+			}
+		}
 
-        private void cancel_Click(object sender, RoutedEventArgs e)
-        {
-            textbox.Undo();
-        }
+		private void cancel_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.Undo();
+		}
 
-        private void cut_Click(object sender, RoutedEventArgs e)
-        {
-            Clipboard.Clear();
-            textbox.Cut();
-        }
+		private void redo_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.Redo();
+		}
 
-        private void copy_Click(object sender, RoutedEventArgs e)
-        {
-            textbox.Copy();
-        }
+		private void cut_Click(object sender, RoutedEventArgs e)
+		{
+			Clipboard.Clear();
+			textbox.Cut();
+		}
 
-        private void insert_Click(object sender, RoutedEventArgs e)
-        {
-            textbox.Paste();
-        }
+		private void copy_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.Copy();
+		}
 
-        private void delete_Click(object sender, RoutedEventArgs e)
-        {
-            textbox.SelectedText = "";
-        }
+		private void insert_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.Paste();
+		}
 
-        private void find_Click(object sender, RoutedEventArgs e)
-        {
+		private void delete_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.SelectedText = "";
+		}
 
-        }
+		private void find_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void find_next_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
+		private void replace_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void find_earlier_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
+		private void go_to_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void replace_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
+		private void highlight_all_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.SelectAll();
+		}
 
-        private void go_to_Click(object sender, RoutedEventArgs e)
-        {
+		private void datetime_Click(object sender, RoutedEventArgs e)
+		{
+			string tmp = Clipboard.GetText();
+			DateTime dateTime = DateTime.Now;
+			Clipboard.SetText(dateTime.ToLocalTime().ToString());
+			textbox.Paste();
+			Clipboard.SetText(tmp);
+		}
 
-        }
+		private void font_style_Click(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Forms.FontDialog fontDialog = new System.Windows.Forms.FontDialog();
 
-        private void highlight_all_Click(object sender, RoutedEventArgs e)
-        {
-            textbox.SelectAll();
-        }
+		}
 
-        private void datetime_Click(object sender, RoutedEventArgs e)
-        {
-            DateTime dateTime = DateTime.Now;
-            textbox.Text += dateTime.ToLocalTime();
-        }
+		private void increase_scale_Click(object sender, RoutedEventArgs e)
+		{
+			if (textbox.FontSize <= 28)
+			{
+				textbox.FontSize += 4;
+			}
+		}
 
-        private void font_style_Click(object sender, RoutedEventArgs e)
-        {
+		private void decrease_scale_Click(object sender, RoutedEventArgs e)
+		{
+			if (textbox.FontSize >= 12)
+			{
+				textbox.FontSize -= 4;
+			}
+		}	
 
-        }
-    }
+		private void default_scale_Click(object sender, RoutedEventArgs e)
+		{
+			textbox.FontSize = 16;
+		}
+
+		private void about_program_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
